e5076badcb2fdd88abe54c810b36abd6ddd64ba1
José Iván Salazar <josesala@espol.edu.ec>
2026-07-27 19:14:59 +0000

    finalizado hoy rendericelosarchivosnoolvidar

--- backend_data_server/settings.py
+++ backend_data_server/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -57,6 +57,7 @@ TEMPLATES = [
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [],
         "APP_DIRS": True,
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.request",
